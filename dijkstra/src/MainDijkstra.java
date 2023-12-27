b537eaff6c9438d4740e2f413816d17a5fa5e095
rowanxgamal <rowangamalelkhouly@gmail.com>
2023-12-27 17:58:53 +0200

    created projects for tree and dijkstra

---
+++ dijkstra/src/MainDijkstra.java
@@ -0,0 +1,6 @@
+public class MainDijkstra {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ tree/src/MainTree.java
@@ -0,0 +1,7 @@
+
+public class MainTree {
+    public static void main(String[] args) {
+
+        }
+    }
+}
